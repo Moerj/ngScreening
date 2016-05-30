71f6a48dc16c500598ccbc8317e43daef5b604fe
Moer <199525807@qq.com>
2016-05-30 10:30:05 +0800

    update

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # ngScreening v0.0.1
 
-angular帅选器组件
+angular筛选器组件
--- js/index.js
+++ js/index.js
@@ -7,6 +7,28 @@ var m = angular.module('APP',['ngScreening']);
 m.controller('testCtrl',function ($scope, $timeout) {
     $scope._hellow = 'hellow scope!!!'
 
+    // ui-select
+    $scope.selectData = [{"id":"1","name":"张三"},{"id":"2","name":"李四"},{
+			"id":"3",
+			"name":"王二"
+		},{
+			"id":"4",
+			"name":"麻子"
+		},{
+			"id":"5",
+			"name":"麻子的哥哥"
+		},{
+			"id":"6",
+			"name":"麻子的姐姐"
+		},{
+			"id":"7",
+			"name":"麻子的姐姐1"
+		},{
+			"id":"8",
+			"name":"麻子的姐姐2"
+		}
+	];
+
     // 初始化过滤组件的总数据
     $scope.data = {};
 
@@ -56,9 +78,11 @@ m.controller('testCtrl',function ($scope, $timeout) {
     ]
 
     $scope.dataCallback = function () {
-        // 利用数据控制筛选器的联动逻辑
+        // 输出数据
         // console.log('吃了梨子就不能吃苹果 !!');
         console.log($scope.data);
+
+        // 利用数据控制筛选器的联动逻辑
         $scope.data.g2[1].isHidden = $scope.data.g1[2].isChecked
 
     }
