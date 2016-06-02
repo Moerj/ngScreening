b2695d2966d0257d492ff78a3b59f1c278cb199b
Moer <199525807@qq.com>
2016-06-02 21:01:30 +0800

    update

--- js/ngScreening.js
+++ js/ngScreening.js
@@ -105,7 +105,7 @@ m.directive('ngScreening',function () {
 })
 
 // 筛选器容器
-m.directive('screening',function (ngScreening) {
+m.directive('screening', ['ngScreening', function (ngScreening) {
     return{
         restrict: 'AE',
         scope: {
@@ -173,7 +173,7 @@ m.directive('screening',function (ngScreening) {
 
         }
     }
-})
+}])
 
 // 自定义筛选组件
 m.directive('screeningDiv',function () {
